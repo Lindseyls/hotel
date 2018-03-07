7f3a1d85932f153192dc4941039e4cf9b515765f
Luxi Lindsey <Luxi.Lindsey@gmail.com>
2018-03-06 16:04:20 -0800

    Created the instance initialize method and also the test for the method.

--- lib/room.rb
+++ lib/room.rb
@@ -1,12 +1,27 @@
 
 module Hotel
-class Room
-  attr_reader
+  class Room
+    attr_reader :room_num, :cost, :status
 
-  def initialize
+    ROOM_NUMBER = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+    ROOM_COST = 200.00
 
-  end
+    def initialize(room_num)
+
+      unless ROOM_NUMBER.include? room_num
+        raise ArgumentError.new("Invalid room number #{room_num}")
+      end
+
+      @room_num = room_num
+      @cost = ROOM_COST
 
 
-end # end of Room class
+      # @status = input[:status] == nil ? :AVAILABLE : input[:status]
+    end
+
+    # def add_reservation(reservation)
+    #   @reservation << reservation
+    # end
+
+  end # end of Room class
 end # end of Hotel module
--- specs/room_spec.rb
+++ specs/room_spec.rb
@@ -1,1 +1,22 @@
 require_relative 'spec_helper'
+
+describe "Room class" do
+  describe "You can create a Room instance" do
+
+    it "Can be created" do
+      (1..20).each do |value|
+        room = Hotel::Room.new(1)
+        room.must_be_instance_of Hotel::Room
+      end
+    end
+
+
+    it "Raises an ArgumentError for invalid parameters" do
+      proc {Hotel::Room.new(0)}.must_raise ArgumentError
+      proc {Hotel::Room.new(21)}.must_raise ArgumentError
+    end
+
+
+
+  end # end of describe "You can create a Room instance"
+end # describe "Admin class"
