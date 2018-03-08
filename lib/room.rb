a567f32c9db585f98f06b9d87038e66e56ea69a6
Luxi Lindsey <Luxi.Lindsey@gmail.com>
2018-03-07 16:58:14 -0800

    Revised room.rb and room_spec.rb to only list the iindividual rooms and cost for one room. The test passed successfully.

--- lib/room.rb
+++ lib/room.rb
@@ -1,22 +1,23 @@
+require 'pry'
 
 module Hotel
   class Room
-    attr_reader :room_num, :cost, :status
+    attr_reader :room_num, :cost, :reservation
 
     ROOM_NUMBER = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
     ROOM_COST = 200.00
 
-    def initialize(room_num)
-
-      unless ROOM_NUMBER.include? room_num
-        raise ArgumentError.new("Invalid room number #{room_num}")
-      end
-
-      @room_num = room_num
+    def initialize(number)
+      @room_num = hotel_room(number)
       @cost = ROOM_COST
+      # @reservation = reservation
+    end
 
-
-      # @status = input[:status] == nil ? :AVAILABLE : input[:status]
+    def hotel_room(number)
+      unless ROOM_NUMBER.include? number
+        raise ArgumentError.new("Invalid room number #{number}")
+      end
+      @room_num = number
     end
 
     # def add_reservation(reservation)
--- specs/room_spec.rb
+++ specs/room_spec.rb
@@ -4,19 +4,22 @@ describe "Room class" do
   describe "You can create a Room instance" do
 
     it "Can be created" do
-      (1..20).each do |value|
-        room = Hotel::Room.new(1)
-        room.must_be_instance_of Hotel::Room
-      end
+      room = Hotel::Room.new(1)
+      room.must_be_instance_of Hotel::Room
     end
 
+    it "Correcty initialize with the cost per room" do
+      room_cost = Hotel::Room.new(9)
+      room_cost.cost.must_equal 200
+    end
 
+  end # end of describe "You can create a Room instance"
+
+  describe "hotel room method" do
     it "Raises an ArgumentError for invalid parameters" do
       proc {Hotel::Room.new(0)}.must_raise ArgumentError
       proc {Hotel::Room.new(21)}.must_raise ArgumentError
     end
+  end # end of describe "hotel room method"
 
-
-
-  end # end of describe "You can create a Room instance"
 end # describe "Admin class"
