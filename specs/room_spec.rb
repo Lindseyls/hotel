9c841ad20782e3a67d58cf6de39456a6e032a43e
Luxi Lindsey <Luxi.Lindsey@gmail.com>
2018-03-08 16:12:16 -0800

    Updated room spec to include a test on the rate method.

--- specs/room_spec.rb
+++ specs/room_spec.rb
@@ -11,8 +11,15 @@ describe "Room class" do
     it "reads the room number" do
       room = Hotel::Room.new(9, 200)
       room.must_respond_to :room_num
-      room.room_num.must_equal 9
       room.room_num.must_be_kind_of Integer
+      room.room_num.must_equal 9
+    end
+
+    it "reads the rate of the room" do
+      room = Hotel::Room.new(9, 200)
+      room.must_respond_to :rate
+      room.rate.must_be_instance_of Integer
+      room.rate.must_equal 200
     end
 
   end # end of describe "You can create a Room instance"
